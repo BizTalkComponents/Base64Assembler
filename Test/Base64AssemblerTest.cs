6fc33ca80d96ce36bc60a3420d980cfbd5e831b3
robinhultman <robin.hultman@ibiz-solutions.se>
2017-03-13 21:29:16 +0100

    Added test

---
+++ Test/Base64AssemblerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Winterdom.BizTalk.PipelineTesting;
+using BizTalkComponents.PipelineComponents.Base64Assembler;
+using System.Xml;
+using System.Text;
+
+namespace BizTalkComponents.Base64Assembler.Tests.UnitTests
+{
+    [TestClass]
+    public class Base64AssemblerTest
+    {
+        [TestMethod]
+        public void TestHappyPath()
+        {
+            var pipeline = PipelineFactory.CreateEmptySendPipeline();
+
+            pipeline.AddDocSpec(typeof(SchemaMock));
+
+            var assembler = new PipelineComponents.Base64Assembler.Base64Assembler
+            {
+                DocumentSpecName = "BizTalkComponents.Base64Assembler.Tests.UnitTests.SchemaMock",
+                DestinationXpath = "/*[local-name()='Root' and namespace-uri()='http://test.SchemaMock']/*[local-name()='Element' and namespace-uri()='']"
+            };
+
+            var message = MessageHelper.CreateFromString("<message></message>");
+
+            pipeline.AddComponent(assembler, PipelineStage.Assemble);
+
+            var result = pipeline.Execute(message);
+
+            var doc = new XmlDocument();
+
+            doc.Load(result.BodyPart.GetOriginalDataStream());
+
+            var node = doc.SelectSingleNode("/*[local-name() = 'Root']/*[local-name() = 'Element']");
+            byte[] data = Convert.FromBase64String(node.InnerText);
+            string decodedString = Encoding.UTF8.GetString(data);
+
+            Assert.AreEqual("<message></message>", decodedString);
+        }
+    }
+}
---
+++ Test/SchemaMock.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.XLANGs.BaseTypes;
+
+namespace BizTalkComponents.Base64Assembler.Tests.UnitTests
+{
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.BizTalk.Schema.Compiler", "3.0.1.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [SchemaType(SchemaTypeEnum.Document)]
+    [Schema(@"http://test.SchemaMock", @"Root")]
+    [System.SerializableAttribute()]
+    [SchemaRoots(new string[] { @"Root" })]
+    public sealed class SchemaMock : Microsoft.XLANGs.BaseTypes.SchemaBase
+    {
+
+        [System.NonSerializedAttribute()]
+        private static object _rawSchema;
+
+        [System.NonSerializedAttribute()]
+        private const string _strSchema = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<xs:schema xmlns=""http://test.SchemaMock"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" targetNamespace=""http://test.SchemaMock"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <xs:element name=""Root"">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element name=""Element"">
+          <xs:simpleType>
+            <xs:restriction base=""xs:base64Binary"" />
+          </xs:simpleType>
+        </xs:element>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+</xs:schema>";
+
+        public SchemaMock()
+        {
+        }
+
+        public override string XmlContent
+        {
+            get
+            {
+                return _strSchema;
+            }
+        }
+
+        public override string[] RootNodes
+        {
+            get
+            {
+                string[] _RootElements = new string[1];
+                _RootElements[0] = "Root";
+                return _RootElements;
+            }
+        }
+
+        protected override object RawSchema
+        {
+            get
+            {
+                return _rawSchema;
+            }
+            set
+            {
+                _rawSchema = value;
+            }
+        }
+    }
+}
