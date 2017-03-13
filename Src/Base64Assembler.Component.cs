412dc6adf0e4504b9073cf54f9df5fd71ba2df85
robinhultman <robin.hultman@ibiz-solutions.se>
2017-03-13 20:52:11 +0100

    Base implementation

--- Src/Base64Assembler.Component.cs
+++ Src/Base64Assembler.Component.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using BizTalkComponents.Utils;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace BizTalkComponents.PipelineComponents.Base64Assembler
 {
     public partial class Base64Assembler
     {
+        public string Name { get { return "Base64Assembler"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description
+        {
+            get
+            {
+                return
+                    "Writes the current body to a new xml schema in base64 encoding.";
+            }
+        }
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("F1368625-0DA0-4BEB-ACD1-AB0D79B2D715");
+        }
+
+        public void InitNew()
+        {
+
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return ValidationHelper.Validate(this, false).ToArray().GetEnumerator();
+        }
+
+        public bool Validate(out string errorMessage)
+        {
+            var errors = ValidationHelper.Validate(this, true).ToArray();
+
+            if (errors.Any())
+            {
+                errorMessage = string.Join(",", errors);
+
+                return false;
+            }
+
+            errorMessage = string.Empty;
+
+            return true;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
     }
 }
--- Src/Base64Assembler.cs
+++ Src/Base64Assembler.cs
@@ -6,15 +6,89 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.BizTalk.Streaming;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Xml;
 
 namespace BizTalkComponents.PipelineComponents.Base64Assembler
 {
     [System.Runtime.InteropServices.Guid("a496e8a7-f8bb-4e55-b7ff-33f6bea70b56")]
     [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
     [ComponentCategory(CategoryTypes.CATID_AssemblingSerializer)]
-    public partial class Base64Assembler
+    public partial class Base64Assembler : Microsoft.BizTalk.Component.Interop.IComponent, IBaseComponent,
+                                        IPersistPropertyBag, IComponentUI
     {
-       
+        private const string DestinationXpathPropertyName = "DestinationXpath";
+        private const string DocumentSpecNamePropertyName = "DocumentSpecName";
 
+        [RequiredRuntime]
+        [DisplayName("Destination xpath")]
+        [Description("Path to the node that should contain the base64 string.")]
+        public string DestinationXpath { get; set; }
+
+        [RequiredRuntime]
+        [DisplayName("DocumentSpecName")]
+        [Description("DocumentSpecName of schema that which the base64 string should be enclosed in.")]
+        public string DocumentSpecName { get; set; }
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            string errorMessage;
+
+            if (!Validate(out errorMessage))
+            {
+                var ex = new ArgumentException(errorMessage);
+                throw ex;
+            }
+
+            DocumentSpec documentSpec;
+
+            try
+            {
+                documentSpec = (DocumentSpec)pContext.GetDocumentSpecByName(DocumentSpecName);
+            }
+            catch (COMException cex)
+            {
+                throw cex;
+            }
+
+            var doc = new XmlDocument();
+
+            using (var sw = new StringWriter(new StringBuilder()))
+            { 
+
+                doc.Load(documentSpec.CreateXmlInstance(sw));
+            }
+
+            var data = pInMsg.BodyPart.GetOriginalDataStream();
+
+            const int bufferSize = 0x280;
+            const int thresholdSize = 0x100000;
+
+            if (!data.CanSeek || !data.CanRead)
+            {
+                data = new ReadOnlySeekableStream(data, new VirtualStream(bufferSize, thresholdSize), bufferSize);
+                pContext.ResourceTracker.AddResource(data);
+            }
+
+            var ms = new MemoryStream();
+            data.CopyTo(ms);
+
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Position = 0;
+
+            var node = doc.SelectSingleNode(DestinationXpath);
+            node.InnerText = Convert.ToBase64String(ms.ToArray());
+
+            var outMs = new MemoryStream();
+            doc.Save(outMs);
+            outMs.Seek(0, SeekOrigin.Begin);
+
+            pInMsg.BodyPart.Data = outMs;
+
+            return pInMsg;
+        }
     }
 }
