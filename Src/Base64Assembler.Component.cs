a13e178866dea2c5484406930bd032c7f9f0fd44
robinhultman <robin.hultman@ibiz-solutions.se>
2017-03-13 14:04:09 +0100

    Initial commit

---
+++ Src/Base64Assembler.Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizTalkComponents.PipelineComponents.Base64Assembler
+{
+    public partial class Base64Assembler
+    {
+    }
+}
---
+++ Src/Base64Assembler.cs
@@ -0,0 +1,20 @@
+﻿using BizTalkComponents.Utils;
+using Microsoft.BizTalk.Component.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizTalkComponents.PipelineComponents.Base64Assembler
+{
+    [System.Runtime.InteropServices.Guid("a496e8a7-f8bb-4e55-b7ff-33f6bea70b56")]
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [ComponentCategory(CategoryTypes.CATID_AssemblingSerializer)]
+    public partial class Base64Assembler
+    {
+       
+
+    }
+}
