7a4283eb45438cc2f278a89d1b1f31eb6f49f158
robinhultman <robin.hultman@ibiz-solutions.se>
2017-03-14 10:32:53 +0100

    Test exception handling

--- Test/Base64AssemblerTest.cs
+++ Test/Base64AssemblerTest.cs
@@ -35,9 +35,51 @@ namespace BizTalkComponents.Base64Assembler.Tests.UnitTests
 
             var node = doc.SelectSingleNode("/*[local-name() = 'Root']/*[local-name() = 'Element']");
             byte[] data = Convert.FromBase64String(node.InnerText);
-            string decodedString = Encoding.UTF8.GetString(data);
+            string decodedString = Encoding.Default.GetString(data);
 
-            Assert.AreEqual("<message></message>", decodedString);
+            Assert.AreEqual("<message></message>", decodedString.Replace("\0",""));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidDocSpecName()
+        {
+            var pipeline = PipelineFactory.CreateEmptySendPipeline();
+
+            pipeline.AddDocSpec(typeof(SchemaMock));
+
+            var assembler = new PipelineComponents.Base64Assembler.Base64Assembler
+            {
+                DocumentSpecName = "Invalid",
+                DestinationXpath = "/*[local-name()='Root' and namespace-uri()='http://test.SchemaMock']/*[local-name()='Element' and namespace-uri()='']"
+            };
+
+            var message = MessageHelper.CreateFromString("<message></message>");
+
+            pipeline.AddComponent(assembler, PipelineStage.Assemble);
+
+            var result = pipeline.Execute(message);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidXpath()
+        {
+            var pipeline = PipelineFactory.CreateEmptySendPipeline();
+
+            pipeline.AddDocSpec(typeof(SchemaMock));
+
+            var assembler = new PipelineComponents.Base64Assembler.Base64Assembler
+            {
+                DocumentSpecName = "BizTalkComponents.Base64Assembler.Tests.UnitTests.SchemaMock",
+                DestinationXpath = "/*[local-name()='RootInvalid' and namespace-uri()='http://test.SchemaMock']/*[local-name()='Element' and namespace-uri()='']"
+            };
+
+            var message = MessageHelper.CreateFromString("<message></message>");
+
+            pipeline.AddComponent(assembler, PipelineStage.Assemble);
+
+            var result = pipeline.Execute(message);
         }
     }
 }
